2e81ecd16c774bf0a677f5401d157e181f8dd628
Maxim Kleverov <maximkleverov@icloud.com>
2025-06-27 13:25:59 +0300

    map into line done (should not leak)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 13:35:34 by mklevero          #+#    #+#             */
-/*   Updated: 2025/06/26 20:53:10 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/06/27 13:16:23 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,60 +27,67 @@ t_game	*init_base_data(char *input)
 	t_game	*data;
 
 	map_in_line = process_map(input);
-	if (map_in_line == NULL)
-		error_message("so... cannot read file");
+	
 }
 
-char	*process_map(char *input) // leaking af 
+void	error_message(char *message)
+{
+	ft_putstr_fd("Error\n", 2);
+	ft_putendl_fd(message, 2);
+	exit(1);
+}
+
+char	*process_map(char *input)
 {
 	int		fd;
 	char	*solo;
 	char	*joined;
+	char	*tempo;
 
 	fd = open(input, O_RDONLY);
 	if (fd == -1)
 		error_message("so... cannot open file");
-	joined = ft_strdup("");
-	if (joined == NULL)
-		return (NULL);
-	while (1)
+	joined = NULL;
+	solo = get_next_line(fd);
+	while(solo)
 	{
-		solo = get_next_line(fd);
-		if (solo)
+		tempo = join_together(joined, solo);
+		free(solo);
+		if(tempo == NULL)
 		{
-			joined = join(joined, solo);
-			if (joined == NULL)
-			{
-				free(joined);
-				error_message("so... malloc failed");
-			}
-			free(solo);
+			free(joined);
+			error_message("so... malloc failed");
 		}
-		else
-			break ;
+		joined = tempo;
+		solo = get_next_line(fd);
 	}
 	close(fd);
 	return (joined);
-	
 }
 
-char	*join(char *s1, char *s2)
+char	*join_together(char *s1, char *s2)
 {
 	size_t	len_1;
 	size_t	len_2;
-	size_t	len_total;
 	char	*joined;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	len_1 = ft_strlen(s1);
+	if (s1 == NULL)
+		len_1 = 0;
+	else
+		len_1 = ft_strlen(s1);
 	len_2 = ft_strlen(s2);
-	len_total = len_1 + len_2;
-	joined = (char *)malloc(sizeof(char) * (len_total + 1));
+	joined = (char *)malloc(sizeof(char) * (len_1 + len_2 + 1));
 	if (joined == NULL)
+	{
+		free(s1);
 		return (NULL);
-	ft_strlcpy(joined, s1, len_total + 1);
-	ft_strlcat(joined, s2, len_total + 1);
+	}	
+	if (s1 != NULL)
+		memcpy(joined, s1, len_1);
+	memcpy(joined + len_1, s2, len_2);
+	joined[len_1 + len_2] = '\0';
 	free(s1);
 	return (joined);
 }
@@ -99,39 +106,4 @@ void	check_input(int ac, char **av)
 	av_len = ft_strlen(av[1]);
 	if (av_len <= 4 || ft_strncmp(av[1] + av_len - 4, ".ber", 4) != 0)
 		error_message("so... provide a valid .ber file");
-}
-
-void	error_message(char *message)
-{
-	ft_putstr_fd("Error\n", 2);
-	ft_putendl_fd(message, 2);
-	exit(1);
-}
-
-
-char	*get_map_str(char *file_name)
-{
-	int		fd;
-	char	*next_line;
-	char	*map_str;
-
-	map_str = NULL;
-	fd = open(file_name, O_RDONLY);
-	if (fd == -1)
-		error_exit("Cannot open the map 🗺️");
-	while (1)
-	{
-		next_line = get_next_line(fd);
-		if (!next_line)
-			break;
-		map_str = so_strjoin(map_str, next_line);
-		if (!map_str)
-		{
-			free(map_str);
-			error_exit("malloc function broke");
-		}
-		free (next_line);
-	}
-
-	return (map_str);
 }
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: mklevero <mklevero@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 13:52:45 by mklevero          #+#    #+#             */
-/*   Updated: 2025/06/26 17:30:24 by mklevero         ###   ########.fr       */
+/*   Updated: 2025/06/27 13:12:46 by mklevero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,11 @@ int			main(int ac, char **av);
 void		check_input(int ac, char **av);
 
 void		error_message(char *message);
+
+t_game	*init_base_data(char *input);
+char	*process_map(char *input);
+char	*join_together(char *s1, char *s2);
+
+
 
 #endif
